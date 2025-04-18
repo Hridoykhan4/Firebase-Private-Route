e0e01172db3be4ab71ff111eb1665b2adacc1181
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-04-18 18:19:07 +0600

    changes added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
     <div className="hero bg-base-200 min-h-[calc(100vh-65px)]">
       <div className="hero-content w-full flex-col ">
         <div className="text-center lg:text-left">
-          <h1 className="text-2xl font-bold">Login now!</h1>
+          <h1 className="text-2xl font-bold">Login noww!</h1>
         </div>
         <div className="card  bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <form onSubmit={handleLogin} className="card-body pb-2">
